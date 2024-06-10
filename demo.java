81068e3e829cacac0c566ed9a9075c2229916f9c
RahulNivas <rahulnivas03@gmail.com>
2024-06-10 12:56:39 +0530

    all files

---
+++ demo.java
@@ -0,0 +1,15 @@
+echo "# javabatch1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RahulNivas/javabatch1.git
+git push -u origin main
+
+
+
+
+
+git remote add origin https://github.com/RahulNivas/javabatch1.git
+git branch -M main
+git push -u origin main-m
